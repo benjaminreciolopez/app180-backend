b0dc44b6c875bfbb74eaf2bcc619ab257d3bb9c9
Benjamin <benjaminreciolopez@hotmail.es>
2026-01-01 14:01:14 +0100

    Initial frontend commit for app180

--- src/app.js
+++ src/app.js
@@ -20,12 +20,20 @@ cron.schedule("59 23 * * *", () => ejecutarAutocierre());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://app180-frontend.vercel.app",
-      "https://app180-frontend-git-main-benjamins-projects-1d0caeba.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      const allowed = [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://app180-frontend.vercel.app",
+        "https://app180-frontend-git-main-benjamins-projects-1d0caeba.vercel.app",
+      ];
+
+      if (!origin || allowed.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
