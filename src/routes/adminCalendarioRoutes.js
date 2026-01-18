287b7088d248b619512d9ea65de46d2e4c6fb966
Benjamin <benjaminreciolopez@hotmail.es>
2026-01-18 21:17:42 +0100

    sabado, 3 enero 2026

--- src/routes/adminCalendarioRoutes.js
+++ src/routes/adminCalendarioRoutes.js
@@ -3,10 +3,6 @@ import { authRequired } from "../middlewares/authMiddleware.js";
 import { roleRequired } from "../middlewares/roleRequired.js";
 import { actualizarEstadoAusencia } from "../controllers/ausenciasController.js";
 
-import {
-  getCalendarioAdmin,
-  getEventosCalendarioAdmin,
-} from "../controllers/adminCalendarioController.js";
 import { importarFestivosNager } from "../controllers/adminCalendarioController.js";
 import { getCalendarioIntegradoAdmin } from "../controllers/adminCalendarioIntegradoController.js";
 
@@ -14,10 +10,6 @@ const router = Router();
 
 router.use(authRequired);
 router.use(roleRequired("admin"));
-
-// 📅 calendario empresa / empleado
-router.get("/calendario", getCalendarioAdmin);
-router.get("/calendario/eventos", getEventosCalendarioAdmin);
 
 router.patch("/ausencias/:id/estado", actualizarEstadoAusencia);
 
