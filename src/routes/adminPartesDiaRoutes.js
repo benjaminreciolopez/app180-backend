88f4f71c32f466ed65153460bbd61a9ec7ccabac
Benjamin <benjaminreciolopez@hotmail.es>
2026-02-14 11:27:11 +0100

    feat: enhanced template management

--- src/routes/adminPartesDiaRoutes.js
+++ src/routes/adminPartesDiaRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getPartesDia, validarParte } from "../controllers/adminPartesDiaController.js";
+import { getPartesDia, validarParte, validarPartesMasivo } from "../controllers/adminPartesDiaController.js";
 
 const router = Router();
 
 router.get("/partes-dia", getPartesDia);
 router.patch("/partes-dia/validar", validarParte);
+router.patch("/partes-dia/validar-masivo", validarPartesMasivo);
 
 export default router;
